23505c1086a594441fdfced2cf469a983aa126d1
peter atonga <peteratonga@gmail.com>
2024-07-01 23:14:45 +0300

    looping

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+function writeCards (arr,event){
+    let newArr = [];
+    for(let i= 0; i<arr.length;i++){
+        newArr.push(`Thank you, ${arr[i]}, for the wonderful ${event} gift!`)
+    }
+    return newArr
+
+}                                                                                              
+
+writeCards(["Guadalupe", "Ollie", "Aki"],'suprise gift!')
+
+function countDown(num){
+    let count = 0;
+    while(num >= count){
+        console.log(num)
+        num--
+    }
+
+}
+countDown(10)
